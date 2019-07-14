36f5fa29dee514b8b1dda083ee50237569a5f0fd
yanyujianshen <ljh274163788@gmail.com>
2019-07-14 16:13:40 +0800

    easy peasy

--- Puzzle4.java
+++ Puzzle4.java
@@ -10,9 +10,9 @@ public class Puzzle4 {
 
     // Matrix A
     static double[][] MATRIX_A = {{10.2, 9.4, 13.6, 15.2},
-				  {20.4, 48.3, 25.4, 5.6},
-				  {11.2, 16.4, 22.4, 14.8},
-				  {9.2, 54.2, 18.8, 20.4}};
+				                  {20.4, 48.3, 25.4, 5.6},
+				                  {11.2, 16.4, 22.4, 14.8},
+				                  {9.2, 54.2, 18.8, 20.4}};
     
     // Matrix B
     static double[][] MATRIX_B = {{9.2, 9.4, 12.6, 14.2},
@@ -26,23 +26,45 @@ public class Puzzle4 {
     }
 
     private static double calcValue(double[][] m1, double[][] m2) {
-	// IMPLEMENT METHOD
-	return 0.0;
+            
+	return sumDiagonal(transpose(subtract(m1,m2)));
     }
     
     private static double[][] subtract(double[][] m1, double[][] m2) {
-	// IMPLEMENT METHOD
-	return null;
+	   if(m1.length != m2.length){
+        return null;
+       } else{
+          for(int i = 0; i < m1.length; i++){
+             for(int j = 0; j < m1[i].length; j++){
+                m1[i][j] = m1[i][j] + m2[i][j];
+             }
+          }
+          return m1;
+       }
+	
     }
 
     private static double[][] transpose(double[][] m) {
-	// IMPLEMENT METHOD
-	return null;
+	   for(int i = 0; i < m.length; i++){
+          for(int j = i; j < m[i].length;j++){
+                swap(m[i][j],m[j][i]);
+          }
+       }
+	return m;
     }
 
+    private static void swap(double a, double b){
+        double temp = a;
+        a = b;
+        b = temp;
+
+    }
     private static double sumDiagonal(double[][] m) {
-	// IMPLEMENT METHOD
-	return 0.0;
+	    double sum = 0.0;
+        for(int i = 0; i < m.length;i++){
+            sum += m[i][i];
+        }
+	return sum;
     }
 
 }
